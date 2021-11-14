bd24dd7e245ac6ec83c41d653a6da74be879edb2
ShahinSalimov <mrsalimofff@gmail.com>
2021-11-15 00:28:03 +0400

    arrayin uzunlugunu tapmaq....

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Massivin Uzunluğunu almaq üçün C# Proqramını yazın
+            int[] ededler = new int[] { 2, 4, 6, 7, 8, 4, 3, 2, 3, 4 };
+            int arrayuzunlug = ededler.Length;
+            Console.WriteLine("massivin uzunlugu {0}-dir..", arrayuzunlug);
+            Console.ReadKey();
+        }
+    }
+}
