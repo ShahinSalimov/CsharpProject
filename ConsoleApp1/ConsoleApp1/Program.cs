d7669b9eaac6f801066ea88ec002cd528d517270
ShahinSalimov <mrsalimofff@gmail.com>
2021-11-15 00:05:46 +0400

    tek ve ya cut ededlerin yoxlanilmasi

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //İstifadəçi tərəfindən daxil edilmiş tam ədədin tək və ya cüt olduğunu yoxlamaq üçün C# proqramı
+            Console.WriteLine("zehmet olmazsa bir eded daxil edin : ");
+            int eded = Convert.ToInt32(Console.ReadLine());
+            if (eded % 2 == 0)
+            {
+                Console.WriteLine("daxil edilen eded cutdur..");
+            }
+            else
+            {
+                Console.WriteLine("daxil edilen eded tekdir..");
+
+            }
+            Console.ReadKey();
+        }
+    }
+    
+       
+
+}
