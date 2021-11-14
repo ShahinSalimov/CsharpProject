928c44363b20cb452463ba8459cc3b112fea4005
ShahinSalimov <mrsalimofff@gmail.com>
2021-11-15 01:20:31 +0400

    arrayde menfi ededleri cap etmek

---
+++ ConsoleApp6/ConsoleApp6/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Massivdəki bütün mənfi elementləri çap etmək üçün C# proqramı yazın
+            int[] arr = { -2, -34, -56, 3, 5, 6, 7, 8, 912, 34, -35, -98 };
+            Console.WriteLine("butun menfi ededler:");
+            foreach (var item in arr)
+            {
+                if (item<0)
+                {
+                    Console.Write(item);
+
+                }
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
