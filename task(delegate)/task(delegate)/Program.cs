972960c65d3cde07623083df2e58490f2e8e773a
ShahinSalimov <mrsalimofff@gmail.com>
2021-11-21 08:56:12 +0400

    ededin tek ve ya cut olmasi,sozde reqemin olub-olmamasi yekunlashdi.(EXTENSION METHOD)

---
+++ task(delegate)/task(delegate)/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_ExtentionMethods1_
+{
+
+    public static class ExtentionMethods
+    {
+        //IsOdd() - int obyektler ucun hemin obyektin tek eded olub olmadigini geri qaytaran
+        //(true/false) method
+        public static bool IsOdd(this int a)
+        {
+
+            return a % 2 != 0;
+        }
+        //IsEven() - int obyektler ucun hemin obyektin cut eded olub olmadigini geri qaytaran
+        //(true/false) method
+        public static bool IsEven(this int a)
+        {
+
+            return a % 2 == 0;
+        }
+        //IsContainsDigit() - string obyektler ucun hemin obyektin daxilinde digit charactre
+        //olub olmamsini geri qaytaran(true/false) metod
+        //public bool IsContainsDigit(string a)
+        public static bool IsContainsDigit(this string b)
+        {
+            bool isdigit=b.Any(x => char.IsDigit(x));
+            return isdigit;
+
+        }
+
+        class Program
+        {
+            static void Main(string[] args)
+            {
+
+                int a = 10;
+                string b = "Baki32Kend";
+
+                bool result = a.IsOdd();
+                bool result1 = a.IsEven();
+                bool result2 = b.IsContainsDigit();
+
+                Console.WriteLine("eded tekdir? Netice : {0}", result);
+                Console.WriteLine("eded cutdur? Netice : {0}", result1);
+                Console.WriteLine("sozde reqem varmi? Netice : {0}", result2);
+
+                Console.ReadKey();
+            }
+
+
+        }
+    }
+}
+
